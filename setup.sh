93d7e695d12517e6d258cc2209bab26c916f237b
Gasoline <gasoline2255@gmail.com>
2025-09-13 23:50:33 +0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip wheel setuptools
-pip install -r requirements.txt
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip install vllm
